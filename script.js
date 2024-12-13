858d129ad8fb6bbe2503783735880eafdfc6d155
PavlovaK19 <pavlovakarolina2004@gmail.com>
2024-12-13 09:08:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: '#app',
+    data: {
+        num1: 0,
+        num2: 0,
+        result: 0,
+        logs: [],
+    },
+    methods: {
+        subtract() {
+            this.result = this.num1 - this.num2;
+            this.addLog(`${this.num1} - ${this.num2}`, this.result);
+        },
+        addLog(input, result) {
+            this.logs.unshift({ input, result });
+            if (this.logs.length > 10) {
+                this.logs.pop();
+            }
+        },
+        clearLogs() {
+            this.logs = [];
+        },
+    },
+});
